9f7b0be0d47c4269c691565c5b6ae0288c386656
dayeon <dayeon0952@gmail.com>
2024-12-06 14:42:37 +0900

    Dy Controller,DTO,Repository 작성/register 생성

---
+++ src/main/java/com/example/demo/DYdto/DyDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.DYdto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DyDTO {
+
+    private Long num;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+}
--- src/main/java/com/example/demo/controller/DyController.java
+++ src/main/java/com/example/demo/controller/DyController.java
@@ -1,14 +1,38 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.DYdto.DyDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Log4j2
+@RequiredArgsConstructor
+
 public class DyController {
 
+
+
+    @GetMapping("/dy/register")
+    public String regisertGet(DyDTO dyDTO){
+
+
+        return "team/dy/register";
+
+    }
+
+    @PostMapping("/dy/register")
+    public String registerPost(DyDTO dyDTO){
+
+        log.info("컨트롤러로 들어온 값" + dyDTO);
+
+
+
+        return "team/dy/list";
+    }
 
 
     @GetMapping("/dy/list")
