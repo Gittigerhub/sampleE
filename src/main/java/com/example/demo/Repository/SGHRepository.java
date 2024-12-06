17deac8028951af9200542bc77b0b8d6e68f43d4
flatchino <tla6063801@gmail.com>
2024-12-06 12:53:15 +0900

    심기훈 커밋입니다3

---
+++ src/main/java/com/example/demo/Repository/SGHRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repository;
+
+import com.example.demo.entity.SGHEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SGHRepository extends JpaRepository<SGHEntity,Long> {
+
+
+
+}
--- src/main/java/com/example/demo/entity/SGHEntity.java
+++ src/main/java/com/example/demo/entity/SGHEntity.java
@@ -1,10 +1,33 @@
 package com.example.demo.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 
 public class SGHEntity {
 
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private  String writer;
 
 
 }
