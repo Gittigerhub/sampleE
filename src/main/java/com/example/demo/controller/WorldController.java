1578c3c592fb23c6cbe93f2394ca4829e57fee3e
wwwwwwwwworld <08sungyu@gmail.com>
2024-12-06 14:36:46 +0900

    조은세계 확인용

--- src/main/java/com/example/demo/controller/WorldController.java
+++ src/main/java/com/example/demo/controller/WorldController.java
@@ -1,20 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.Repository.WorldRepository;
+import com.example.demo.entity.WorldEntity;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/team/world")
 public class WorldController {
 
-    @GetMapping("/team/world/list")
-    public String list(){
+    private final WorldRepository worldRepository;
+
+    @GetMapping("/list")
+    public String list(WorldEntity worldEntity){
 
         log.info("깃 확인용" +
                 "controller" +
                 "입니다" +
                 "!!!");
+
+        worldRepository.save(worldEntity);
 
         return "team/world/list";
     }
